fd73ab86ab00e248c6faef90f186a1887e396f1c
isaacmuchunu <isaacmuchunu@gmail.com>
2021-08-06 14:42:18 +0300

    my progress on post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,84 +2,102 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view ('index',['post'=>$posts]);
-    }
+/**
+* Display a listing of the resource.
+*
+* @return \Illuminate\Http\Response
+*/
+public function index()
+{
+$posts = Post::latest()->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
+return view('posts.index', ['posts' => $posts]);
 }
+
+/**
+* Show the form for creating a new resource.
+*
+* @return \Illuminate\Http\Response
+*/
+public function create()
+{
+return view('posts.create');
+}
+
+/**
+* Store a newly created resource in storage.
+*
+* @param \Illuminate\Http\Request $request
+* @return \Illuminate\Http\Response
+*/
+public function store(Request $request)
+{
+Post::create($this->validatePost());
+
+return redirect('/posts');
+}
+
+/**
+* Display the specified resource.
+*
+* @param \App\Post $post
+* @return \Illuminate\Http\Response
+*/
+public function show(Post $post)
+{
+
+return view('posts.show', ['post' => $post]);
+}
+
+/**
+* Show the form for editing the specified resource.
+*
+* @param \App\Post $post
+* @return \Illuminate\Http\Response
+*/
+public function edit(Post $post)
+{
+
+return view('posts.edit', compact('post'));
+}
+
+/**
+* Update the specified resource in storage.
+*
+* @param \Illuminate\Http\Request $request
+* @param \App\Post $post
+* @return \Illuminate\Http\Response
+*/
+public function update(Request $request, Post $post)
+{
+$post->update($this->validatePost());
+
+return redirect('/posts/' .$post->id);
+}
+
+/**
+* Remove the specified resource from storage.
+*
+* @param \App\Post $post
+* @return \Illuminate\Http\Response
+*/
+public function destroy(Post $post)
+{
+$post->delete($this->validatePost());
+return redirect('/posts');
+}
+
+protected function validatePost()
+{
+return request()->validate([
+'title' => 'required',
+'slug' => 'required',
+'body' => 'required'
+]);
+}
+}--
