fa5c7ac0b4d1eab988e510343b77c352b746befd
isaacmuchunu <isaacmuchunu@gmail.com>
2021-08-06 10:01:48 +0300

    my database migrations  and models

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
+
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -12,5 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get( '$posts/{post}', 'PostsController@show');
+Route::get('/posts/{post}', 'PostsController@show');
